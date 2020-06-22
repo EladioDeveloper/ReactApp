92244935c530a2d101224cab8aa4080c169d0e5b
Eladio Rodriguez <eladiojuniorrodriguez@gmail.com>
2020-06-22 02:30:54 -0400

    HMW 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,6 +6,7 @@ import Footer from "./FooterComponent";
 import Contact from "./ContactComponent";
 import Home from "./HomeComponent";
 import DishDetail from "./DishdetailComponent";
+import About from "./AboutComponent";
 import { DISHES } from "../shared/dishes";
 import { COMMENTS } from "../shared/comments";
 import { PROMOTIONS } from "../shared/promotions";
@@ -68,6 +69,11 @@ class Main extends Component {
             )}
           />
           <Route exact path="/contactus" component={Contact} />
+          <Route
+            exact
+            path="/aboutus"
+            component={() => <About leaders={this.state.leaders} />}
+          />
           <Redirect to="/home" />
         </Switch>
         <Footer />
